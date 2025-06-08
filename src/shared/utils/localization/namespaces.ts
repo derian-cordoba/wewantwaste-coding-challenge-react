1f856379d3169577fde10eb317f039efc51aea70
derian-cordoba <derianricardo451@gmail.com>
2025-06-07 21:52:39 -0600

    chore(shared): added some namespace comments

--- src/shared/utils/localization/namespaces.ts
+++ src/shared/utils/localization/namespaces.ts
@@ -2,4 +2,6 @@ export enum Namespace {
   SELECT_SKIP = "select-skip",
 }
 
+// All namespaces used in the application
+// e.g. ["select-skip", "another-namespace-file-name"]
 export const namespaces = Object.values(Namespace);
