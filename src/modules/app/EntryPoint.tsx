ff90df8cd9ac7cec454039d974c2e1090d83a9cd
derian-cordoba <derianricardo451@gmail.com>
2025-06-07 19:24:33 -0600

    feat(root): created the base architecture

---
+++ src/modules/app/EntryPoint.tsx
@@ -0,0 +1,9 @@
+import { PageLayout } from "@/shared/components/Layouts/PageLayout";
+
+export function EntryPoint() {
+  return (
+    <PageLayout>
+      <p>Hello World!</p>
+    </PageLayout>
+  );
+}
